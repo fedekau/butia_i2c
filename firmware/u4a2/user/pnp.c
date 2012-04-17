906e602fd88850046aeb5ff30aaeaa7e2b4fa2db
John Pereira <jpereira@fing.edu.uy>
2012-04-17 12:00:58 -0300

    Add PnP archive, start implement tabe with resistance values

---
+++ firmware/u4a2/user/pnp.c
@@ -0,0 +1,17 @@
+
+
+#include "user/pnp.h"
+
+
+void initTableDeviceIdResistance(void){
+
+    table_device_id_resistance_value[0].nombre = {"boton"};
+    table_device_id_resistance_value[0].resValue = R_BOTON;
+
+    table_device_id_resistance_value[1].nombre = {"grises"};
+    table_device_id_resistance_value[1].resValue = R_GRISES;
+
+    table_device_id_resistance_value[2].nombre = {"dist"};
+    table_device_id_resistance_value[2].resValue = R_DIST;
+
+}
---
+++ firmware/u4a2/user/pnp.h
@@ -0,0 +1,41 @@
+/*******************************************************************************/
+#ifndef PNP_H
+#define PNP_H
+
+#include "typedefs.h"
+
+/*******************************************************************************/
+
+
+#define R_LED 700
+#define R_BOTON 700
+#define R_GRISES 700
+#define R_DIST 700
+#define R_LUZ 700
+#define R_ACCELEROMETER 700
+#define R_TEMPERATURE 700
+#define R_MAGNETIC 700
+#define R_MICROPHONE 700
+#define R_BUZZER 700
+#define R_TILT 700
+#define R_VIBRATION 700
+#define R_POTENTIOMETER 700
+#define R_CAMERA 700
+#define R_MOTION 700
+#define R_DCMOTOR 700
+
+#define MAX_DEVICES 9 /*Max cant of senssor/actuator type*/
+
+typedef struct   {
+    char nombre[8];
+    WORD resValue;
+}  device_resistance_value;
+
+/*Remeber to chnge the old maping for that*/
+extern rom device_resistance_value table_device_id_resistance_value[MAX_DEVICES];
+
+
+
+
+
+#endif PNP_H
