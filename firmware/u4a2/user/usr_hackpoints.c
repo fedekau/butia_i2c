b851568e29ed17c11e2145e6214d7c2477f3563b
Ayle <aricca@fing.edu.uy>
2012-11-29 16:01:31 -0200

    renamed hackp to user module

--- firmware/u4a2/user/usr_hackpoints.c
+++ firmware/u4a2/user/usr_hackpoints.c
@@ -9,7 +9,7 @@
 #include <delays.h>
 #include "system/typedefs.h"
 #include "system/usb/usb.h"
-#include "user/hackpoints.h"
+#include "user/usr_hackpoints.h"
 #include "io_cfg.h"              /* I/O pin mapping */
 #include "user/handlerManager.h"
 #include "dynamicPolling.h"   
@@ -26,13 +26,12 @@ void HackPointsProcessIO(void);
 void HackPointsInit(byte i);
 void HackPointsReceived(byte*, byte, byte);
 void HackPointsRelease(byte i);
-void HackPointsConfigure(byte);
 
 /* Table used by te framework to get a fixed reference point
  * to the user module functions defined by the framework
 /** USER MODULE REFERENCE*****************************************************/
 #pragma romdata user
-const uTab HackPointsModuleTable = {&HackPointsInit, &HackPointsRelease, &HackPointsConfigure, "hackp"};
+const uTab HackPointsModuleTable = {&HackPointsInit, &HackPointsRelease, "hackp"};
 #pragma code
 
 /** D E C L A R A T I O N S **************************************************/
@@ -73,25 +72,6 @@ void HackPointsInit(byte handler) {
     TRISDbits.RD6 = IN;
     TRISDbits.RD7 = IN;
 }/*end UserLedAmarilloInit*/
-
-/******************************************************************************
-/* Function:        HACK_POINTSConfigure(void)
- *
- * PreCondition:    None
- *
- * Input:           None
- *
- * Output:          None
- *
- * Side Effects:    None
- *
- * Overview:        This function sets the specific configuration for the user module, it is called by the framework 
- *                        
- *
- * Note:            None
- *****************************************************************************/
-void HackPointsConfigure(byte handler) {
-}
 
 void HackPointsProcessIO(void) {
 }/*end ProcessIO */
@@ -152,6 +132,11 @@ void HackPointsReceived(byte* recBufferHackPoints, byte len, byte handler) {
             ((HACK_POINTS_DATA_PACKET*) sendBufferHackPoints)->_byte[1] = HACK_POINTS_MINOR_VERSION;
             ((HACK_POINTS_DATA_PACKET*) sendBufferHackPoints)->_byte[2] = HACK_POINTS_MAJOR_VERSION;
             HackPointsCounter = 0x03;
+            break;
+
+        case WRITE:
+            ((HACK_POINTS_DATA_PACKET*) sendBufferHackPoints)->_byte[0] = ((HACK_POINTS_DATA_PACKET*) recBufferHackPoints)->_byte[0];
+            HackPointsCounter = 0x01;
             break;
 
         case SET_HIGH_PINS:
