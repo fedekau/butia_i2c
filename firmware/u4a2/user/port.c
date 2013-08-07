9b848cc29ae7dd281c97d778294f23086a8abaeb
Alan Aguiar <alanjas@hotmail.com>
2013-08-06 21:58:52 -0300

    add forgot const

--- firmware/u4a2/user/port.c
+++ firmware/u4a2/user/port.c
@@ -29,7 +29,7 @@ void PortRelease(byte i);
 // Table used by te framework to get a fixed reference point to the user module functions defined by the framework 
 /** USER MODULE REFERENCE*****************************************************/
 #pragma romdata user
-uTab PortModuleTable = {&PortInit, &PortRelease, "port"};
+const uTab PortModuleTable = {&PortInit, &PortRelease, "port"};
 #pragma code
 
 /** D E C L A R A T I O N S **************************************************/
